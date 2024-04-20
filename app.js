17a4e8c430cdd3e2a7332a463681fc8edbb33ba7
Its Mishi <misbah154233@gmail.com>
2024-04-20 07:05:33 +0500

    PractiseDone

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1.Write a script to greet your website visitor using JS alert
+// box.
+alert("Welcome");
+// 2. Write a script to display following message on your web
+// page:
+alert("Error! Please enter a valid password");
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+alert("Welcome to JS land....\nHappy coding" )
+// 4.Write a script to display following messages in sequence:
+alert("Welcome to JS land...." )
+alert("Happy Coding" )
+
+alert("Hello from the browser's developer console!");
