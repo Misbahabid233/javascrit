c083251c424c485685e1bcfdc17607a09e5d348b
Misbahabid233 <misbah154233@gmail.com>
2024-05-04 00:50:55 -0300

    Done

---
+++ Chapter4/app.js
@@ -0,0 +1,43 @@
+// 1. Declare 3 variables in one statement.
+var Name = ("Misbah");
+var fatherName = ("Abid");
+var nationalitization = ("Pakistani");
+console.log(Name , fatherName , nationalitization);
+// 2. Declare 5 legal & 5 illegal variable names.
+var Name = ("Misbah");
+var fatherName = ("Abid");
+var nationalitization = ("Pakistani");
+var phone_number = ("09876543");
+var profession = ("Student");
+console.log(Name , fatherName , nationalitization ,phone_number,profession);
+var erty = ("cvbnm");
+var fghj = ("fghjk");
+var cvbnm = ("tyyu");
+var fghj = ("fghjk");
+var hjkb = ("fghj");
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// / c) Variables must begin with a ______, ______ or
+// // _____. For example $name, _name or name/
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+// a) A heading stating “Rules for naming JS variables”
+document.write("<h1>Rules for naming JS variables </h1>");
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+var userName = ("Misbah");
+var $my_1stVariable = ("Hmm");
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+var $name = ("fish");
+var _num = ("4567");
+// d) Variable names are case _________
+document.write (" Variable names are case sensitive </br>")
+// e) Variable names should not be JS _________
+document.write ("Variable names should not be JS keyword")
+
+
+
---
+++ Chapter5/app.js
@@ -0,0 +1,24 @@
+// 1. Write a program that take two numbers & add them in a
+// new variable. Show the result in your browser.
+var sum = (5+3);
+console.log(sum);
+document.write("<h1>Sum of 5 and 3 is 8 </h1>");
+// 2. Repeat task1 for subtraction, multiplication, division &
+// modulus.
+// subtraction
+var subtraction = (5-3);
+console.log(subtraction);
+document.write("<h1>Subtraction of 5 and 3 is 2 </h1>");
+
+// multiplication
+var multiplication = (5*3);
+console.log(multiplication);
+document.write("<h1>Multiplication of 5 and 3 is 15 </h1>");
+// division
+var division = (5/3);
+console.log(division);
+document.write("<h1>Division of 5 and 3 is 1.6666 </h1>");
+// modulus
+var modulus = (5%3);
+console.log(modulus);
+document.write("<h1>Modulous of 5 and 3 is 2 </h1>");
